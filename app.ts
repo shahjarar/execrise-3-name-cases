dad5d1cf98dcd31728384d3fb63ac56584922933
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 13:46:15 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,11 @@
+// lower case
+let personName: string = "Baber"
+console.log("lowercase:", personName.toLowerCase());
+
+
+// upper case
+console.log("uppercase:", personName.toLocaleUpperCase);
+
+
+// Title case
+console.log("titlecase:", personName.replace(/\bw/g,c => c.toUpperCase()));
